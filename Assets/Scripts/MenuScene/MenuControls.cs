efff23204f995490d3883bed7afc01c720c8f734
alexstak <alex.stak@yandex.ru>
2022-04-01 16:00:47 +0300

    main menu

---
+++ Assets/Scripts/MenuScene/MenuControls.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+}
